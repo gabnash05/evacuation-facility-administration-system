afab5469cf891cbff41332886ead9c0899f048a1
KIM GABRIEL NASAYAO <kimgabriel.nasayao@g.msuiit.edu.ph>
2025-10-23 15:21:44 +0800

    EFASS-25: fix: incorrect import order

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,13 +4,14 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+
 from app.models.user import User
 
 # from app.models.event import Event
 # from app.models.center import EvacuationCenter
 # from app.models.household import Household
 # from app.models.individual import Individual
-
-db = SQLAlchemy()
-migrate = Migrate()
-jwt = JWTManager()
